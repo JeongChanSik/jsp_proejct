9070658e8b455839132f4a7677773af3d844d27c
JeongChanSik <ckstlrdlfksp@naver.com>
2023-10-20 14:03:09 +0900

    아이디가 null값 또는 빈 값일 때 테스트 통과 못 하도록  설정

--- src/test/java/models/member/JoinService.java
+++ src/test/java/models/member/JoinService.java
@@ -1,7 +1,18 @@
 package models.member;
 
-public class JoinService {
-    public void join(Member member){
+import commons.BadRequestException;
+import commons.Validator;
 
+public class JoinService {
+    // JoinValidator 객체 생성
+    private Validator validator;
+    private MemberDAO memberdao;
+
+    public JoinService(Validator validator, MemberDAO memberdao){
+        this.validator = validator;
+        this.memberdao = memberdao;
+    }
+    public void join(Member member){
+        validator.check(member);
     }
 }
--- src/test/java/tests/JoinServiceTest.java
+++ src/test/java/tests/JoinServiceTest.java
@@ -1,11 +1,15 @@
 package tests;
 
+import commons.BadRequestException;
 import models.member.JoinService;
+import models.member.JoinValidator;
 import models.member.Member;
+import models.member.MemberDAO;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName("회원가입 기능 단위 테스트")
 public class JoinServiceTest {
@@ -13,7 +17,7 @@ public class JoinServiceTest {
     private JoinService joinService;
 
     void init(){
-        joinService = new JoinService();
+        joinService = new JoinService(new JoinValidator(), new MemberDAO());
     }
 
     private Member getMember(){
@@ -32,7 +36,21 @@ public class JoinServiceTest {
         assertDoesNotThrow(() ->{
             joinService.join(getMember());
         });
+    }
+    @Test
+    @DisplayName("필수 항목 검증(아이디, 비밀번호, 비밀번호 확인, 회원명, 이메일, 회원가입 약관 동의)" +
+            " 검증 실패 시 BadRequestException 발생")
+    void requiredFieldCheck(){
+        // 아이디가 null값 또는 빈 값일 때
+        assertThrows(BadRequestException.class, () -> {
+            Member member = getMember();
 
+            member.setUserId(null);
+            joinService.join(member);
 
+            member.setUserId("  ");
+            joinService.join(member);
+
+        });
     }
 }
