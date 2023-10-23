d1b632e3f8a2b35b2b59dcd8bde383ef1789f60a
JeongChanSik <ckstlrdlfksp@naver.com>
2023-10-23 15:22:14 +0900

    기능 통합 - 회원 가입 완료

--- src/main/java/controllers/member/JoinController.java
+++ src/main/java/controllers/member/JoinController.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import static commons.ScriptUtil.alertError;
+import static commons.ScriptUtil.go;
 
 @WebServlet("/member/join")
 public class JoinController extends HttpServlet {
@@ -28,6 +29,9 @@ public class JoinController extends HttpServlet {
         try {
             JoinService service = ServiceManager.getInstance().joinService();
             service.join(req);
+
+            String url = req.getContextPath() + "/member/login";
+            go(resp, url, "parent");
 
             // resp.sendRedirect(req.getContextPath() + "/member/login");
         } catch (RuntimeException e) {
